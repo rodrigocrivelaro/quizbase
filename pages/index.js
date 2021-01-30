cf3435be2dd4c7197e3c473dfaebec8723feffa7
Rodrigo Crivelaro <rreiscrivelaro@gmail.com>
2021-01-29 21:20:56 -0300

    Alterações referentes as aulas 2 e 3

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,8 @@ import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -17,66 +19,62 @@ import GitHubCorner from '../src/components/GitHubCorner';
 //   background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
-    width: 100%;
-    max-width: 350px;
-    padding-top: 45px;
-    margin: auto 10%;
-    @media screen and (max-width: 500px) {
-        margin: auto;
-        padding: 15px;
-    }
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `;
 
 export default function Home() {
-    const router = useRouter();
-    const [name, setName] = React.useState('');
+  const router = useRouter();
+  const [name, setName] = React.useState('');
 
-    return (
-        <QuizBackground backgroundImage={db.bg}>
-            <Head>
-                <title>AluraQuiz - Modelo Base</title>
-            </Head>
-            <QuizContainer>
-                <QuizLogo />
-                <Widget>
-                    <Widget.Header>
-                        <h1>The legend of zelda</h1>
-                    </Widget.Header>
-                    <Widget.Content>
-                        <form onSubmit={function (infosDoEvento) {
-                            infosDoEvento.preventDefault();
-                            router.push(`/quiz?name=${name}`);
-                            console.log('Fazendo uma submissão por meio do react');
-                        }}
-                        >
-                            <input
-                                onChange={function (infosDoEvento) {
-                                    console.log(infosDoEvento.target.value);
-                                    // State
-                                    // name = infosDoEvento.target.value;
-                                    setName(infosDoEvento.target.value);
-                                }}
-                                placeholder="Diz ai seu nome"
-                            />
-                            <button type="submit" disabled={name.length === 0}>
-                                Jogar
-                {name}
-                            </button>
-                        </form>
-                    </Widget.Content>
-                </Widget>
+  return (
+      <QuizBackground backgroundImage={db.bg}>
+          <Head>
+              <title>{db.title}</title>
+          </Head>
+          <QuizContainer>
+              <QuizLogo />
+              <Widget>
+                  <Widget.Header>
+                      <h1>{db.title}</h1>
+                  </Widget.Header>
+                  <Widget.Content>
+                      <p>{db.description}</p>
+                      <form onSubmit={function (infosDoEvento) {
+                        infosDoEvento.preventDefault();
+                        router.push(`/quiz?name=${name}`);
+                      }}
+                      >
+                          <Input
+                            name="nomeDoUsuario"
+                            onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                            placeholder="Diz ai seu nome"
+                            value={name}
+                          />
+                          <Button type="submit" disabled={name.length === 0}>
+                              {`Jogar ${name}`}
+                          </Button>
+                      </form>
+                  </Widget.Content>
+              </Widget>
 
-                <Widget>
-                    <Widget.Content>
-                        <h1>Quizes da Galera</h1>
+              <Widget>
+                  <Widget.Content>
+                      <h1>Quizes da Galera</h1>
 
-                        <p>lorem ipsum dolor sit amet...</p>
-                    </Widget.Content>
-                </Widget>
-                <Footer />
-            </QuizContainer>
-            <GitHubCorner projectUrl="https://github.com/omariosouto" />
-        </QuizBackground>
-    );
+                      <p>lorem ipsum dolor sit amet...</p>
+                  </Widget.Content>
+              </Widget>
+              <Footer />
+          </QuizContainer>
+          <GitHubCorner projectUrl="https://github.com/rodrigocrivelaro" />
+      </QuizBackground>
+  );
 }
